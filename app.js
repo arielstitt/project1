4c3158892b621283c0b2e0b2f4d5cbdbec06c938
arielstitt <arielstitt@gmail.com>
2018-02-05 18:49:39 -0500

    got all buttons to work

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 // alert('this is working')
+
+// The buttons is linked to an onclick function
 $(`#100-points`).click(function(){   
     prompt("these 2 numbers added together equals 4")
 })
+
+$(`#200-points`).click(function(){   
+    prompt("An animal with a three letter word that uses a litter box")
+})
+
+$(`#300-points`).click(function(){   
+    prompt("A three letter word that is used to refer to a mother")
+})
+
+$(`#400-points`).click(function(){   
+    prompt("99 is before this number")
+})
+
+$(`#500-points`).click(function(){   
+    prompt("H2O is another name for this essential life force")
+})
+
