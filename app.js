c947184e21c4678ad5e48d5286202a7004d8633d
arielstitt <arielstitt@gmail.com>
2018-02-05 20:26:42 -0500

    added if else statements to all the buttons, added a color to the buttons and the background

--- app.js
+++ app.js
@@ -24,22 +24,37 @@ alert("SHAME! SHAME! SHAME!")
 
 $(`#200-points`).click(function(){   
     const question = prompt("An animal with a three letter word that uses a litter box")
-    if(){
-
+    if(question === "cat" || question === "Cat" || question === "CAT"){
+        alert("Who's a pretty kitty?")
     }else {
-        alert("wow...just wow")
+        alert("A cat will chew through your macbook cord...")
     }
 })
 
 $(`#300-points`).click(function(){   
-    prompt("A three letter word that is used to refer to a mother")
+    const question = prompt("A three letter word that is used to refer to a mother")
+    if(question === "mom" || question === "Mom" || question === "MOM"){
+        alert("When was the last time you called her?")
+    }else {
+        alert("She would be so disapointed")
+    }
 })
 
 $(`#400-points`).click(function(){   
-    prompt("99 is before this number")
+    const question = prompt("99 is before this number")
+    if (question === "100") {
+        alert("Well, I'll be damned. You can math!")
+    } else {
+        alert("well, this is embarrassing")
+    }
 })
 
 $(`#500-points`).click(function(){   
-    prompt("H2O is another name for this essential life force")
+    const question = prompt("H2O is another name for this essential life force")
+    if (question === "water" || question === "Water" || question === "WATER") {
+        alert("Drink up, ho!")
+    } else {
+        alert("Cape town would be disapointed ")
+    }
 })
 
