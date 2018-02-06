439e69f9e55b05f95021db57bda913165ab5ae5c
arielstitt <arielstitt@gmail.com>
2018-02-06 11:16:39 -0500

    added multiple choice to the prompt

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // alert('this is working')
-
 // The buttons is linked to an onclick function
 $(`#100-points`).click(function(){  
 // The answer 
@@ -12,10 +11,14 @@ $(`#100-points`).click(function(){
     // }
 
 // 
-    const question = prompt("These two numbers added together equals 4")
+    const question = prompt(`These two numbers added together equals 4 
+    a. 2 
+    b. 7
+    c. 9
+    `)
     console.log(question)
-if(question === "2") {
-alert("You is kind. You is smart. Is important")
+if(question === "a") {
+alert(`You is kind. You is smart. Is important`)
 } else {
 alert("SHAME! SHAME! SHAME!")
 
@@ -23,8 +26,12 @@ alert("SHAME! SHAME! SHAME!")
 })
 
 $(`#200-points`).click(function(){   
-    const question = prompt("An animal with a three letter word that uses a litter box")
-    if(question === "cat" || question === "Cat" || question === "CAT"){
+    const question = prompt(`An animal with a three letter word that uses a litter box
+    a. trash panda
+    b. cat
+    c. hairless molerat`)
+
+    if(question === "b"){
         alert("Who's a pretty kitty?")
     }else {
         alert("A cat will chew through your macbook cord...")
@@ -32,8 +39,11 @@ $(`#200-points`).click(function(){
 })
 
 $(`#300-points`).click(function(){   
-    const question = prompt("A three letter word that is used to refer to a mother")
-    if(question === "mom" || question === "Mom" || question === "MOM"){
+    const question = prompt(`A three letter word that is used to refer to a mother
+    a. cat
+    b. dog
+    c. mom`)
+    if(question === "c"){
         alert("When was the last time you called her?")
     }else {
         alert("She would be so disapointed")
@@ -41,8 +51,11 @@ $(`#300-points`).click(function(){
 })
 
 $(`#400-points`).click(function(){   
-    const question = prompt("99 is before this number")
-    if (question === "100") {
+    const question = prompt(`99 is before this number
+    a. One hundred twenty-five thousand sic hundred minutes
+    b. peanuts
+    c. 100 `)
+    if (question === "c") {
         alert("Well, I'll be damned. You can math!")
     } else {
         alert("well, this is embarrassing")
@@ -50,8 +63,11 @@ $(`#400-points`).click(function(){
 })
 
 $(`#500-points`).click(function(){   
-    const question = prompt("H2O is another name for this essential life force")
-    if (question === "water" || question === "Water" || question === "WATER") {
+    const question = prompt(`H2O is another name for this essential life force
+    a. boogers
+    b. water
+    c. lactose`)
+    if (question === "b") {
         alert("Drink up, ho!")
     } else {
         alert("Cape town would be disapointed ")
