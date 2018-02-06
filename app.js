76ef168d9052175ac7528ffe38eb210f8645ba61
arielstitt <arielstitt@gmail.com>
2018-02-06 16:57:15 -0500

    added science cat and all buttons work

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
     //     alert("YOU SHOULD BE ASHAMED!")
     // }
 
-// food and wine question 100 pts
+// FOOD AND WINE CAT QUESTIONS
 $(`#fnw-100`).click(function(){
         const question = prompt(`fnw question 100
         a. 
@@ -66,59 +66,59 @@ $(`#fnw-500`).click(function(){
             alert(`incorrect`)
         }
     })
-// $(`.200-points`).click(function(){   
-//     const question = prompt(`An animal with a three letter word that uses a litter box
-//     a. trash panda
-//     b. cat
-//     c. hairless molerat`)
-
-//     if(question === "b"){
-//         alert("Who's a pretty kitty?")
-//     }else {
-//         alert("A cat will chew through your macbook cord...")
-//     }
-// })
-
-// $(`#three`).click(function(){   
-//     const question = prompt(`A three letter word that is used to refer to a mother
-//     a. cat
-//     b. dog
-//     c. mom`)
-//     if(question === "c"){
-//         alert("When was the last time you called her?")
-//     }else {
-//         alert("She would be so disapointed")
-//     }
-// })
-
-// $(`.400-points`).click(function(){   
-//     const question = prompt(`99 is before this number
-//     a. One hundred twenty-five thousand sic hundred minutes
-//     b. peanuts
-//     c. 100 `)
-//     if (question === "c") {
-//         alert("Well, I'll be damned. You can math!")
-//     } else {
-//         alert("well, this is embarrassing")
-//     }
-// })
-
-// $(`.500-points`).click(function(){   
-//     const question = prompt(`H2O is another name for this essential life force
-//     a. boogers
-//     b. water
-//     c. lactose`)
-//     if (question === "b") {
-//         alert("Drink up, ho!")
-//     } else {
-//         alert("Cape town would be disapointed ")
-//     }
-// })
-// $(`#final-answer`).click(function(){   
-//     const question = prompt("How much wood could a woodchuck chuck if a woodchuck couldn't chuck wood?")
-//     if (question === "42") {
-//         alert("I cant belive you guessed it!")
-//     } else {
-//         alert("maybeeeeee?")
-//     }
-// })
+// SCIENCE CAT QUESTIONS
+$(`#sci-100`).click(function(){
+    const question = prompt(`sci question 100
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
+$(`#sci-200`).click(function(){
+    const question = prompt(`sci question 200
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
+$(`#sci-300`).click(function(){
+    const question = prompt(`sci question 300
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
+$(`#sci-400`).click(function(){
+    const question = prompt(`sci question 400
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
+$(`#sci-500`).click(function(){
+    const question = prompt(`sci question 500
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
