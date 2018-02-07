b6d60a0ca8727309d973405bb6ad0deab4213eaa
arielstitt <arielstitt@gmail.com>
2018-02-07 11:23:09 -0500

    added .answers, .wong-answer, .right-answers to css

--- app.js
+++ app.js
@@ -11,26 +11,23 @@
     // }
 
 // FOOD AND WINE CAT QUESTIONS
-$(`#fnw-100`).click(function(){
-        const question = prompt(`fnw question 100
-        a. 
-        b.
-        c.`)
-        if (question === "a") {
-            alert(`correct`)
-        } else {
-            alert(`incorrect`)
-        }
-    })
+
 $(`#fnw-200`).click(function(){
         const question = prompt(`fnw question 200
         a. 
         b.
         c.`)
-        if (question === "a") {
+if (question === "a") {
             alert(`correct`)
+//   thisi is where I am removing this class and adding a class
+//   if user answer the question correctly change the color of the btn to purple
+          $(`#fnw-200`).removeClass(`answers`)
+          $(`#fnw-200`).addClass(`right-answer`)
         } else {
             alert(`incorrect`)
+//  if the user answers the question incorrectly, change the color to red
+           $(`#fnw-200`).removeClass(`answers`)
+          $(`#fnw-200`).addClass(`wrong-answer`)
         }
     })
 $(`#fnw-300`).click(function(){
@@ -38,10 +35,14 @@ $(`#fnw-300`).click(function(){
         a. 
         b.
         c.`)
-        if (question === "a") {
+  if (question === "a") {
             alert(`correct`)
+          $(`#fnw-300`).removeClass(`answers`)
+          $(`#fnw-300`).addClass(`right-answer`)
         } else {
             alert(`incorrect`)
+           $(`#fnw-300`).removeClass(`answers`)
+          $(`#fnw-300`).addClass(`wrong-answer`)
         }
     })
 $(`#fnw-400`).click(function(){
@@ -49,10 +50,14 @@ $(`#fnw-400`).click(function(){
         a. 
         b.
         c.`)
-        if (question === "a") {
+ if (question === "a") {
             alert(`correct`)
+          $(`#fnw-400`).removeClass(`answers`)
+          $(`#fnw-400`).addClass(`right-answer`)
         } else {
             alert(`incorrect`)
+           $(`#fnw-400`).removeClass(`answers`)
+          $(`#fnw-400`).addClass(`wrong-answer`)
         }
     })    
 $(`#fnw-500`).click(function(){
@@ -60,10 +65,14 @@ $(`#fnw-500`).click(function(){
         a. 
         b.
         c.`)
-        if (question === "a") {
+if (question === "a") {
             alert(`correct`)
+          $(`#fnw-500`).removeClass(`answers`)
+          $(`#fnw-500`).addClass(`right-answer`)
         } else {
             alert(`incorrect`)
+           $(`#fnw-500`).removeClass(`answers`)
+          $(`#fnw-500`).addClass(`wrong-answer`)
         }
     })
 // SCIENCE CAT QUESTIONS
@@ -170,6 +179,62 @@ $(`#anm-400`).click(function(){
 })
 $(`#anm-500`).click(function(){
     const question = prompt(`anm question 500
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
+// TV and cinema category
+$(`#tnc-100`).click(function(){
+    const question = prompt(`tnc question 100
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
+$(`#tnc-200`).click(function(){
+    const question = prompt(`tnc question 200
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
+$(`#tnc-300`).click(function(){
+    const question = prompt(`tnc question 300
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
+$(`#tnc-400`).click(function(){
+    const question = prompt(`tnc question 400
+    a. 
+    b.
+    c.`)
+    if (question === "a") {
+        alert(`correct`)
+    } else {
+        alert(`incorrect`)
+    }
+})
+$(`#tnc-500`).click(function(){
+    const question = prompt(`tnc question 500
     a. 
     b.
     c.`)
